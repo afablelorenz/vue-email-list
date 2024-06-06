ead4eff7fabd08f66e04c132215c732a7109e232
afablelorenz <afablelorenz@gmail.com>
2024-06-06 09:45:04 +0200

    updated script

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ createApp({
     },
     methods:{
         generateEmail: function(){
-            this.randomEmail= [];
             for(let i=0; i<10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
